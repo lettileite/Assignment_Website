54d66ca7174a7f70e41b7394de2b4b6072a689d5
lettileite <34384710+lettileite@users.noreply.github.com>
2020-08-18 09:46:27 +1200

    secondcommit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <meta name="keywords" content="Bike, trip, Biketour">
             <meta name="author" content="Leticia de Oliveira Leite">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Bike Trips I want to do</title>
+            <title>Kaio na Web</title>
         
         <?php
             include 'external_libraries.html';
@@ -16,29 +16,48 @@
         
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Kaio's Web</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Kaio and Ben</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <header>
-             
-        </header>
-        <section id="content"> 
-            <div class="container">
-                <h1>Content</h1>
-                <div class="row">
-                    <div class="col-6">
-                        <p>
-                            hello! This is Leticia
-                        </p>
-                    </div>
-                    <div class="col-6">
-                        <img src="img/download.jpeg" alt="bike">
+            <section id="content"> 
+                <div class="container">
+                    <h1>Kaio's history</h1>
+                    <div class="row">
+                        <div class="col-6">
+                            <p>
+                                Hello! This is Kaio.
+                            </p>
+                        </div>
+                        <div class="col-6">
+                            <img src="img/watchingTV.jpg" alt="bike">
+                        </div>
                     </div>
                 </div>
-            </div>
-            
-        </section>
+            </section>
+        </header>
+       
 
         <section id="video">
             <div class="container">
-                <h1>My Video</h1>
+                <h1>Kaio's Video</h1>
                 <div class="embed-responsive embed-responsive-16by9">
                     
                     <iframe class="responsive-iframe" src="video/sample1.mp4"type="video/mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-medi; gyroescope; picture-in-picture" allowfullscreen>
@@ -52,32 +71,33 @@
         </section>
 
         <section id="contacts">
-            
-            <div class="container">
-                <h1>
-                    Contact Me
-                </h1>
-                <form action="#">
-                    <div class="form-group">
-                        <label for="Name"> Name </label>
-                            <input type="text" class="form-control" name="contact-name" placeholder="Enter your name">
-                            </input>
-                    </div>
-                    <div class="form-group">
-                        <label for="Email"> Email </label>
-                            <input type="email" class="form-control" name="contact-email" placeholder="Enter your email">
-                            </input>
-                    </div>
-                    <div class="form-group">
-                        <label for="PhoneNumber"> Phone Number </label>
-                            <input type="number" class="form-control" name="contact-phonenumber" placeholder="Enter your phone number">
-                            </input>
-                    </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
+                
+                <div class="container">
+                    <h1>
+                        Contact Me
+                    </h1>
+                    <form action="#">
+                        <div class="form-group">
+                            <label for="Name"> Name </label>
+                                <input type="text" class="form-control" name="contact-name" placeholder="Enter your name">
+                                </input>
+                        </div>
+                        <div class="form-group">
+                            <label for="Email"> Email </label>
+                                <input type="email" class="form-control" name="contact-email" placeholder="Enter your email">
+                                </input>
+                        </div>
+                        <div class="form-group">
+                            <label for="PhoneNumber"> Phone Number </label>
+                                <input type="number" class="form-control" name="contact-phonenumber" placeholder="Enter your phone number">
+                                </input>
+                        </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
 
-        </section>
+            </section>
+            
         <footer>
 
         </footer>
